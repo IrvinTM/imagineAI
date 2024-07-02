1715e1a8db1c4e1a5edfb4bfb81fdc94d17af243
IrvinTM <irvint698@gmail.com>
2024-07-01 22:29:46 -0600

    links

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -29,7 +29,7 @@ export function StickyNavbar({AppComponent}) {
         color="blue-gray"
         className="p-1 font-normal"
       >
-        <a href="#" className="flex items-center">
+        <a href="https://github.com/IrvinTM/imagineAI.git" className="flex items-center">
           Github
         </a>
       </Typography>
@@ -39,7 +39,7 @@ export function StickyNavbar({AppComponent}) {
         color="blue-gray"
         className="p-1 font-normal"
       >
-        <a href="#" className="flex items-center">
+        <a href="https://github.com/zukijourney/api-docs?tab=readme-ov-file" className="flex items-center">
           How to get api key
         </a>
       </Typography>
